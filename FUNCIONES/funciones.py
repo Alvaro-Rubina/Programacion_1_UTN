1e3cc6e8fe586a20ca0f08d63ff396e45d5c1210
Alvaro Rubina <alvarorubina132@gmail.com>
2023-10-19 09:56:50 -0300

    Archivo donde se almacenarán las funciones

---
+++ FUNCIONES/funciones.py
@@ -0,0 +1,12 @@
+# Sumar digitos de un numero
+
+def add_digits(numb):
+    total = 0
+    actual_numb = 0
+
+    while numb != 0:
+        actual_numb = numb % 10 
+        numb //= 10
+        total += actual_numb
+
+    print(f"Suma total de sus digitos: {total}")
