5f307a014465746fe33ee8451323620398b86c52
Alvaro Rubina <alvarorubina132@gmail.com>
2023-10-19 15:04:52 -0300

    Funciones

---
+++ Ejercicios_en_clase/funciones.py
@@ -0,0 +1,16 @@
+# Sumar digitos de un numero
+def add_digits(numb):
+    total = 0
+    actual_numb = 0
+
+    while numb != 0:
+        actual_numb = numb % 10 
+        numb //= 10
+        total += actual_numb
+
+    print(f"Suma total de sus digitos: {total}")
+
+# Ahorcado
+def hangman():
+    for i in range(6):
+        print(i)
