630328490c1c3167b8206873412ba15a76fb1d15
Alvaro Rubina <alvarorubina132@gmail.com>
2023-10-19 17:03:09 -0300

    Archivo de funciones actualizado - Funciones agregadas

--- Ejercicios_en_clase/funciones.py
+++ Ejercicios_en_clase/funciones.py
@@ -11,6 +11,37 @@ def add_digits(numb):
     print(f"Suma total de sus digitos: {total}")
 
 # Ahorcado
-def hangman():
-    for i in range(6):
-        print(i)
+def hangman(letters_list, word, name):
+    attempts = 5
+
+    while attempts > 0:
+        # Muestro la longitud (los espacios) de la palabra a adivinar
+        print("\n- COLOR:", end = " ")
+        for letter in letters_list:
+            print(letter, end=" ")
+
+        # Muestro los intentos que le quedan al usuario
+        print(f"\n- INTENTOS : {attempts}")
+
+        # Ingreso una letra y verifico si está en la palabra
+        letter = input("\nIngrese una letra: ").lower()
+        if letter in word:
+            print("Correcto!")
+            # Reemplazo el "_" por la letra adivinada para actualizar el tablero
+            for i in range(len(word)):
+                if word[i] == letter:
+                    letters_list[i] = letter
+        else:
+            print("Incorrecto!")
+            attempts -= 1
+
+        # mensaje en caso de haber acertado a todas las letras
+        print("-----------------------------------")
+        if letters_list == list(word):
+            print(f"Felicidades, {name}, adivinaste el color!")
+            break
+
+        # Mensaje en caso de no haber acertado a la palabra (quedarse sin intentos)
+        if attempts == 0:
+            print(f"Mala suerte, {name}, no adivinaste el color ({word})")
+    
