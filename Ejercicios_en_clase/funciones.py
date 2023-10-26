c851848b53a1c6e1e932d2571f71f74a1d176aa8
Alvaro Rubina <alvarorubina132@gmail.com>
2023-10-26 11:32:39 -0300

    Actualizado archivo de funciones

--- Ejercicios_en_clase/funciones.py
+++ Ejercicios_en_clase/funciones.py
@@ -1,3 +1,5 @@
+import random
+
 # Sumar digitos de un numero
 def add_digits(numb):
     total = 0
@@ -44,4 +46,53 @@ def hangman(letters_list, word, name):
         # Mensaje en caso de no haber acertado a la palabra (quedarse sin intentos)
         if attempts == 0:
             print(f"Mala suerte, {name}, no adivinaste el color ({word})")
-    
+
+
+# Bingo
+def adding_numbers_to_card(b_card, ):
+    f = 1
+    numbers = []
+    # Itero cada fila dentro del cartón de bingo
+    for card in b_card:
+        print(f"FILA {f} DEL CARTÓN")
+        f += 1
+        # Itero n en un rango de 5, me permite ingresar 5 numeros por cartón (0 - 4)
+        for n in range(5):
+            while True:
+                # Ingreso el numero
+                number = int(input(f"Numero {n + 1}: "))
+                # Si el numero ya ha sido ingresado antes, se pide nuevamente
+                if number in numbers:
+                    print("El numero ya se ha ingresado, ingrese otro")
+                    continue
+                # Verifico que el numero esté en el rango permitido
+                if (number < 1) or (number > 75):
+                    print("* Numero invalido, por favor ingrese un numero entre 1 y 75")
+                # Si las condiciones anteriores se cumplen, agrego el numero al cartón
+                else:
+                    numbers.append(number)
+                    card.append(number)  
+                    break
+    return b_card
+
+def showing_card(b_card):
+    for card in b_card:
+        print(card) 
+
+def bingo_game(b_card):
+    while True:
+        # Genero una bolilla aleatoria
+        ball = random.randint(1, 75)
+        print(f"\n* BOLILLA: {ball}")
+
+        for card in b_card:
+            for i in range(len(card)):
+                if card[i] == ball:
+                    print("¡Numero encontrado!")
+                    card[i] = "X"
+                    break
+
+        # Condiciones para ganar
+
+# def horizontal(b_card):
+
