a1868bd1f4f3d63cb3b181a38dd56ae6af0e7f93
Alvaro Rubina <alvarorubina132@gmail.com>
2023-11-01 14:30:33 -0300

    PARCIAL 2 | Avance hasta ingreso de ADN

---
+++ PARCIAL_2/mutantes.py
@@ -0,0 +1,49 @@
+from termcolor import colored
+
+# Inicio
+print("<<< SISTEMA DE RECLUTAMIENTO DE MUTANTES >>>")
+print("** Se comprobara, mediante su ADN, si usted es apto para unirse a la Hermandad y ser parte del combate contra "  
+    + "los X-men")
+
+# Ingreso del ADN 
+print("\n** Ingrese su secuencia de ADN.")
+print("\nIMPORTANTE: Cada secuencia de ADN debe componerse de 6 letras, cuyos valores solo pueden ser [A, T, C, G], " 
+    + "representantes de cada base nitrogena del ADN")
+
+# Llenando el array contenedor de las secuencias de ADN
+dna = []
+
+for row in range(6):
+    while True:
+        dna_seq = input(f"- FILA {row + 1} :").upper() 
+
+        # Si el largo de la secuencia es distinto a 6, lo pido nuevamente
+        if len(dna_seq) != 6:
+            print("La secuencia de ADN debe componerse de 6 letras. Ingreselo nuevamente")
+            continue
+        else:
+            # Verifico que la secuencia de ADN solo contenga las letras permitidas
+            valid_seq = True
+
+            for letter in dna_seq:
+                # Analizo la letra actual
+                valid_letter = letter
+
+                # Si alguna de las letras no es válida, hago un "break" y pido la secuencia de nuevo
+                if (valid_letter != 'A' and valid_letter != 'T' and valid_letter != 'C' and valid_letter != 'G'):
+                    print("La secuencia de ADN solo debe contener las letras permitidas [A, T, C o G]")
+                    valid_seq = False
+                    break
+            
+            # Si la secuencia de ADN cumple todas las condiciones, la agrego a la lista
+            if valid_seq:
+                dna.append(dna_seq)
+                break
+
+# Muestro el ADN ingresado
+print("\n** ADN ingresado y procesado correctamente. A continuacion se muestran las secuencias de su ADN **")
+
+for row in dna:
+    for letter in row:
+        print(letter, end= "  ")
+    print()
