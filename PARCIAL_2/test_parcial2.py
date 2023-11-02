0d95f9cac23ac687b0a39a65d27ffbeaa2e5f97b
Alvaro Rubina <alvarorubina132@gmail.com>
2023-11-02 01:22:55 -0300

    Añadidos casos de testeo para secuencias verticales

--- PARCIAL_2/test_parcial2.py
+++ PARCIAL_2/test_parcial2.py
@@ -18,18 +18,21 @@ def test_horizontal_counting(dna, seqs_counter):
 
 """
 
-# Prueba contador de secuencias verticales ---------------------------------------------------------------------
-
+# Prueba contador de secuencias verticales -----------------------------------------------------------------------
+"""
 @pytest.mark.parametrize("dna, seqs_counter",[
     # En caso de que 2 secuencias de ADN contengan 4 o mas letras iguales
     (["ATGCTA", "ATCCTC", "ATAACC", "ATTGGG", "ATCACG", "ATGGTC"], 2),
     # En caso de que el ADN no contenga secuencias de letras iguales
     (["ATCGTA", "TTAGCT", "CCCTAG", "GGATTA", "CCATGT", "CCAATT"], 0),
     # En caso de que todas las secuencias del ADN contengan 4 o mas letras iguales
-    (["AAAAAA", "TTTTTT", "CCCCCC", "GGGGGG", "AAAAAA", "CCCCCC"], 6),
+    (["CCCCCC", "CCCCCC", "CCCCCC", "CCCCCC", "CCCCCC", "CCCCCC"], 6),
     # En caso de que 1 secuencia de ADN contengan 4 o mas letras iguales (la ultima letra de cada string)
     (["AAAAAT", "CTGATT", "TTTCGT", "CACAGT", "TTCACT", "ATTGAT"], 1),
     ])
 def test_vertical_counting(dna, seqs_counter):
     assert funciones.vertical_counting(dna) == seqs_counter
 
+"""
+
+# Prueba contador de secuencias diagonales -----------------------------------------------------------------------
