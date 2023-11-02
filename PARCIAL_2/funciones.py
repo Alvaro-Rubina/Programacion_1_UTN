90c9c895728c4e3c4efaabf1a1a6afdabe23c3fb
Alvaro Rubina <alvarorubina132@gmail.com>
2023-11-02 14:57:09 -0300

    Modificaciones a funcion vertical_counting

--- PARCIAL_2/funciones.py
+++ PARCIAL_2/funciones.py
@@ -1,56 +1,52 @@
-# FUNCIONES NECESARIAS PARA PARCIAL 2 -----------------------------------------------------------------------------
+# FUNCIONES PARCIAL 2 -----------------------------------------------------------------------------
 
-# Funcion que verifica si el ADN es mutante segun el numero de secuencias
+# Verificacion de gen mutante o humano
 def is_mutant(dna):
-    # Variables
     mutant = False
     horizontal_sequences = 0
     vertical_sequences = 0
     diagonal_sequences = 0
 
     # Conteo de secuencias
-    # SECUENCIAS HORIZONTALES
     horizontal_sequences = horizontal_counting(dna)
-    # SECUENCIAS VERTICALES
     vertical_sequences = vertical_counting(dna)
-    # SECUENCIAS DIAGONALES
     diagonal_sequences = diagonal_counting(dna)
 
     
     # SECUENCIAS TOTALES
     total = horizontal_sequences + vertical_sequences + diagonal_sequences
 
-    # Verifico si es mutante o no dependiendo el numero de secuencias (para caso TRUE, total > 1)
+    # Verificacion si es mutante segun numero de secuencias
     if total > 1:
         mutante = True
 
     return mutant
 
-# Funcion que cuenta el numero de SECUENCIAS HORIZONTALES
+# SECUENCIAS HORIZONTALES
 def horizontal_counting(dna):
     seqs_counter = 0
     letter_counter = 0
 
-    # Recorro las secuencias (filas) de la lista
     for row in dna:
         letter_counter = 1
-        # Recorro las letras de cada secuencia (empiezo por la segunda letra, porque comparo con la letra anterior)
         for letter in range(1, len(row)):
             current_letter = row[letter]
             previous_letter = row[letter - 1]
-            
+
             # Reviso si hay una secuencia de 1 o mas letras iguales
             if current_letter == previous_letter:
                 letter_counter += 1
 
             # Si se forma una secuencia de letras igual o mayor a 4, sumo 1 al contador de secuencias
-            if letter_counter >= 4:
+            if letter_counter == 4:
                 seqs_counter += 1
                 break
     
     return seqs_counter
 
-# Funcion que cuenta el numero de SECUENCIAS VERTICALES (importante revisar, reparar funcionamiento)
+
+
+# SECUENCIAS VERTICALES 
 def vertical_counting(dna):
     seqs_counter = 0
     letter_counter = 0
@@ -58,7 +54,7 @@ def vertical_counting(dna):
     # Recorro cada columna
     for column in range(6):
         letter_counter = 1
-        # Recorro las secuencias (filas) del ADN, empezando por el indice 1 para compararlo con su anterior (0)
+        # Recorro las filas
         for row in range(1, len(dna)):
             current_letter = dna[row][column]
             previous_letter = dna[row - 1][column]
@@ -70,8 +66,36 @@ def vertical_counting(dna):
                 letter_counter = 1
 
             # Si se forma una secuencia de letras igual o mayor a 4, sumo 1 al contador de secuencias
-            if letter_counter >= 4:
+            if letter_counter == 4:
                 seqs_counter += 1
                 break
     
+    return seqs_counter
+
+
+
+# SECUENCIAS DIAGONALES
+def diagonal_counting(dna):
+    seqs_counter = 0
+    letter_counter = 0
+
+    # Diagonales Izquierda a derecha (SEGUIR REVISANDO, NO RETORNA BIEN ULTIMO CASO DE TESTEO)
+    for i in range(len(dna)):
+        for j in range(len(dna[i])):
+            letter_counter = 1
+            current_letter = dna[i][j]
+
+            while i < len(dna) - 1 and j < len(dna[i]) - 1:
+                next_letter = dna[i + 1][j + 1]
+
+                if current_letter == next_letter:
+                    letter_counter += 1
+
+                    if letter_counter == 4:
+                        seqs_counter += 1
+                        break
+
+                i += 1
+                j += 1
+
     return seqs_counter
