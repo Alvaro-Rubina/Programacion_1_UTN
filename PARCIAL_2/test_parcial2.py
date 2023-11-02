29a0acdd4347780b4a1ea52d932c282174e276ef
Alvaro Rubina <alvarorubina132@gmail.com>
2023-11-02 14:57:52 -0300

    Añadidos casos de testeo

--- PARCIAL_2/test_parcial2.py
+++ PARCIAL_2/test_parcial2.py
@@ -19,15 +19,16 @@ def test_horizontal_counting(dna, seqs_counter):
 """
 
 # Prueba contador de secuencias verticales -----------------------------------------------------------------------
+
 """
 @pytest.mark.parametrize("dna, seqs_counter",[
-    # En caso de que 2 secuencias de ADN contengan 4 o mas letras iguales
+    # 2 secuencias, 4 o mas letras iguales
     (["ATGCTA", "ATCCTC", "ATAACC", "ATTGGG", "ATCACG", "ATGGTC"], 2),
-    # En caso de que el ADN no contenga secuencias de letras iguales
+    # Sin secuencias de letras iguales
     (["ATCGTA", "TTAGCT", "CCCTAG", "GGATTA", "CCATGT", "CCAATT"], 0),
-    # En caso de que todas las secuencias del ADN contengan 4 o mas letras iguales
+    # todas las secuencias, 4 o mas letras iguales
     (["CCCCCC", "CCCCCC", "CCCCCC", "CCCCCC", "CCCCCC", "CCCCCC"], 6),
-    # En caso de que 1 secuencia de ADN contengan 4 o mas letras iguales (la ultima letra de cada string)
+    # 1 secuencia, 4 o mas letras iguales 
     (["AAAAAT", "CTGATT", "TTTCGT", "CACAGT", "TTCACT", "ATTGAT"], 1),
     ])
 def test_vertical_counting(dna, seqs_counter):
@@ -36,3 +37,13 @@ def test_vertical_counting(dna, seqs_counter):
 """
 
 # Prueba contador de secuencias diagonales -----------------------------------------------------------------------
+
+@pytest.mark.parametrize("dna, seqs_counter",[
+    (["ATGCGA", "CAGTTC", "TTATAT", "AGAAGG", "CCCCAA", "TCACTG"], 1),
+    (["ATCGAT", "ATCGAT", "ATCGAT", "ATCGAT", "ATCGAT", "ATCGAT"], 0),
+    (["ACGATT", "TACCGA", "CTATGC", "TCTAGG", "CGCTAT", "CACAGC"], 2),
+    (["ACATGA", "TACACT", "GTACAG", "AGTACA", "TTGTGT", "CCAGTA"], 5)
+    ])
+def test_diagonal_counting(dna, seqs_counter):
+    assert funciones.diagonal_counting(dna) == seqs_counter
+
